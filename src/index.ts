2cbea25bb6487281b70ac4b1392116180cea51f2
eduard <eduardcamayo13@gmail.com>
2023-12-13 10:59:57 -0500

    Parte 2: GET Y POST listo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express"
+import diaryRouter from "./routes/diaries"
 
 const app = express()
 app.use(express.json())
@@ -8,6 +9,8 @@ const PORT = 3000
     console.log("Heloooo friend")
     res.send("Buenos diasss")
  })
+
+app.use("/api/diaries", diaryRouter)
 
  app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
